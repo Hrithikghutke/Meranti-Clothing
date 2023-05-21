6f09d4afbc96c08c66708e36c9fa408cd4f74ec9
Hrithik <hrithikghutke01@gmail.com>
2023-05-21 18:12:19 +0530

    Done Till Order Api's _ will be starting frontend

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -44,6 +44,7 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: [true, "Please Enter Product Stock"],
     maxLength: [4, "Stock Cannot Exceed 4 Characters"],
+    default: 1,
   },
   numOfReviews: {
     type: Number,
